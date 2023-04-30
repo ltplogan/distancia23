14acfeee45496e288ab67b17b2c79ca37939f348
Laurent Toledo <ltp.lorenzo@gmail.com>
2023-04-30 13:59:27 +0200

    Práctica PHP PhpDocumentor

---
+++ js/searchIndex.js
@@ -0,0 +1,44 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\sumar\u0028\u0029",
+            "name": "sumar",
+            "summary": "",
+            "url": "namespaces/default.html#function_sumar"
+        },                {
+            "fqsen": "\\dividir\u0028\u0029",
+            "name": "dividir",
+            "summary": "",
+            "url": "namespaces/default.html#function_dividir"
+        },                {
+            "fqsen": "\\Calculadora",
+            "name": "Calculadora",
+            "summary": "",
+            "url": "classes/Calculadora.html"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003A__construct\u0028\u0029",
+            "name": "__construct",
+            "summary": "",
+            "url": "classes/Calculadora.html#method___construct"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Aget_sumar\u0028\u0029",
+            "name": "get_sumar",
+            "summary": "Devuelve\u0020una\u0020suma",
+            "url": "classes/Calculadora.html#method_get_sumar"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003A\u0024n1",
+            "name": "n1",
+            "summary": "",
+            "url": "classes/Calculadora.html#property_n1"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003A\u0024n2",
+            "name": "n2",
+            "summary": "",
+            "url": "classes/Calculadora.html#property_n2"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
